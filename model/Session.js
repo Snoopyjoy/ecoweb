d397a079ccf31ad7e7231735a6ff5335bb02f0c2
HeXiaoliang <hel13140302@126.com>
2018-11-27 12:39:56 +0800

    不适用数据库保存session

--- model/Session.js
+++ model/Session.js
@@ -3,8 +3,6 @@
  */
 const JWT = require('jsonwebtoken');
 const CODES = require("../ErrorCodes");
-const RedisSessionPayload = require("./adapter/RedisSessionPayload");
-const MongoSessionPayload = require("./adapter/MongoSessionPayload");
 
 function Session() {
     if (!Session.$instance) Session.$instance = this;
@@ -13,82 +11,9 @@ function Session() {
 
 Session.prototype.init = function (params) {
     this.config = params || {};
-    this.config.prefix = this.config.prefix || "";
-    let ins = this;
-    if (this.config.onePointEnter) {
-        this.formatKey = function (id, token) {
-            return ins.config.prefix + "user_sess_" + id + "_0";
-        }
-        this.checkSess = function (id, token, sess) {
-            return sess.userid == id && sess.token == token;
-        }
-    } else {
-        this.formatKey = function (id, token) {
-            return ins.config.prefix + "user_sess_" + id + "_" + token;
-        }
-        this.checkSess = function (id, token, sess) {
-            return true;
-        }
-    }
-    if (params.payloadWorker) {
-        this.payloadWorker = params.payloadWorker;
-    }
-    if (!this.payloadWorker) {
-        if (this.config.storage === "mongodb") {
-            this.payloadWorker = new MongoSessionPayload();
-        } else {
-            this.payloadWorker = new RedisSessionPayload();
-        }
-    }
-    this.payloadWorker.session = this;
 }
 
-Session.prototype.savePayload = function (key, payload, expireTime) {
-    return this.payloadWorker.savePayload(key, payload, expireTime);
-}
-
-Session.prototype.updateUserAllPayloads = function (userid, extra) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let keys = await this.findAllPayloadKeys(userid);
-            if (keys) {
-                let expireTime = this.config.tokenExpireTime;
-                for (let i = 0; i < keys.length; i++) {
-                    let newExtra;
-                    let oldExtra = await this.readPayload(keys[i]);
-                    if (oldExtra instanceof Array && extra instanceof Array) {
-                        newExtra = [ ...oldExtra, ...extra ];
-                    } else {
-                        newExtra = { ...oldExtra, ...extra };
-                    }
-                    await this.savePayload(keys[i], newExtra, expireTime);
-                }
-            }
-            resolve();
-        } catch (err) {
-            reject(err);
-        }
-    });
-}
-
-Session.prototype.readPayload = function (key) {
-    return this.payloadWorker.readPayload(key);
-}
-
-Session.prototype.removePayload = function (key) {
-    return this.payloadWorker.removePayload(key);
-}
-
-Session.prototype.findAllPayloadKeys = function (userid) {
-    return this.payloadWorker.findAllPayloadKeys(userid);
-}
-
-Session.prototype.refreshPayloadExpireTime = function (key, expireTime) {
-    return this.payloadWorker.refreshPayloadExpireTime(key, expireTime);
-}
-
-Session.prototype.save = function (user, extra, callBack) {
-
+Session.prototype.save = function (user, extra ) {
     callBack = arguments[arguments.length - 1];
     if (typeof callBack !== "function") callBack = null;
     extra = typeof extra === "object" ? extra : null;
@@ -99,92 +24,24 @@ Session.prototype.save = function (user, extra, callBack) {
     let expireTime = this.config.tokenExpireTime;
 
     let sess = { ...extra };
+    sess.id = userID;
     sess.userid = userID;
     sess.type = user.type;
     sess.loginTime = loginTime;
-
-    let token = JWT.sign({ id: sess.userid, time: loginTime }, this.config.secret, { expiresIn: expireTime });
-
-    let ins = this;
-    return new Promise((resolve, reject) => {
-        const key = ins.formatKey(userID, loginTime);
-        ins.savePayload(key, sess, expireTime).then(() => {
-            if (callBack) return callBack(null, token);
-            resolve(token);
-        }).catch(err => {
-            console.error(err);
-            if (callBack) return callBack(err);
-            reject(err);
-        });
-    });
-}
-
-Session.prototype.remove = function (user, callBack) {
-    let id = (user.id ? user.id : user.userid) || user._id;
-    let key = this.formatKey(id, user.loginTime);
-    return new Promise((resolve, reject) => {
-        this.removePayload(key).then(() => {
-            if (callBack) return callBack();
-            resolve();
-        }).catch(err => {
-            console.error(err);
-            if (callBack) return callBack(err);
-            reject(err);
-        });
-    });
-}
-
-Session.prototype.removeAll = function (userid, callBack) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let keys = await this.findAllPayloadKeys(userid);
-            for (let i = 0; i < keys.length; i++) {
-                await this.removePayload(keys[i]);
-            }
-        } catch (err) {
-            console.error(err);
-            if (callBack) return callBack(err);
-            reject(err);
-        }
-        resolve();
-    });
-}
-
-Session.prototype.refresh = function (user, extra, callBack) {
-
-    callBack = arguments[arguments.length - 1];
-    if (typeof callBack !== "function") callBack = null;
-    extra = typeof extra === "object" ? extra : null;
-
-    return new Promise(async (resolve, reject) => {
-        try {
-            await this.remove(user);
-            let token = this.save(user, extra);
-            if (callBack) return callBack(null, token);
-            resolve(token);
-        } catch (err) {
-            console.error(err);
-            if (callBack) return callBack(err);
-            reject(err);
-        }
-    });
+    return JWT.sign({ id: sess.userid, time: loginTime }, this.config.secret, { expiresIn: expireTime });
 }
 
 Session.prototype.check = function (auth, callBack) {
     return new Promise(async (resolve, reject) => {
-        // invalid auth - synchronous
         try {
             if (!this.config.secret) {
                 throw Error.create(CODES.SESSION_ERROR, 'session is not configed correctly');
             }
 
-            let decoded = JWT.verify(auth, this.config.secret);
-            let key = this.formatKey(decoded.id, decoded.time);
-            let payload = await this.readPayload(key);
-            if (!payload) {
+            let payload = JWT.verify(auth, this.config.secret);
+            if (!payload || !String(payload.id).hasValue() ) {
                 throw Error.create(CODES.SESSION_ERROR, 'auth expired or invalid');
             }
-
             if (callBack) return callBack(null, payload);
             resolve(payload);
         } catch (err) {
